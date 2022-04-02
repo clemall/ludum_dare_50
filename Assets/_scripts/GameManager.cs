4493c43b1974c41d6028c8cba8c5c1aa8d851b65
clemall <clementallardpro@gmail.com>
2022-04-02 12:49:38 +0200

    Init project

---
+++ Assets/_scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    public bool stopGame = false;
+
+    public GameObject gameOverScreen;
+
+
+    private static GameManager _instance;
+    public static GameManager instance
+    {
+        get
+        {
+            if(_instance == null)
+                _instance = GameObject.FindObjectOfType<GameManager>();
+            return _instance;
+        }
+    }
+
+
+    public void gameOver(){
+        stopGame = true;
+    }
+
+    void Start(){
+    }
+
+    void Update()
+    {
+        if (stopGame)
+        {
+            gameOverScreen.SetActive(true);
+        }
+    }
+
+}
+
+
